979d7efb22f414e0798c6efec6adfa0c337a2c87
CakarOnur <CakarOnur@users.noreply.github.com>
2021-02-10 07:36:09 +0300

    Dictonary added.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> City = new Dictionary<int, string>();
+
+            City.Add(9, "Aydın");
+            City.Add(35, "İzmir");
+            City.Add(42, "Konya");
+            City.Add(16, "Bursa");
+
+            Console.Write("Enter your car plate:");
+            int Plaka = Convert.ToInt16(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(City[Plaka]);
+            }
+            catch
+            {
+                Console.WriteLine("City is not found...");
+            }
+        }
+    }
+}
